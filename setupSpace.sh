0791cabb2521178bcf5806559cd15370f613bacc
HarrisonJM <empty.box@hotmail.com>
2018-09-28 15:07:17 +0100

    first commit

---
+++ setupSpace.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+git clone https://github.com/HarrisonJM/DeviceDrivers.git
+git clone https://github.com/HarrisonJM/EmbeddedSystemsDevelopment
+git clone https://github.com/HarrisonJM/BaPOS
+git clone https://github.com/HarrisonJM/ParallelComputing
